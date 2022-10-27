96d4cf61c9ad2646ac408b05d8f583e79ed29535
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2022-10-27 13:57:58 -0700

    Finally Holy wow

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,8 +4,14 @@ public class FizzBuzz
     public void run() {
         for (int i = 1; i <= 100; i++)
         {
-            if ( i/10.0 == 5 || i/10.0 == 0 ) {
+            if (i/3.0 == i/3 && i/5.0 == i/5) {
+                System.out.println("FizzBuzz");
+            } else if (i/3.0 == i/3) {
+                System.out.println("Fizz");
+            } else if (i/5.0 == i/5) {
                 System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
         }
     }    
