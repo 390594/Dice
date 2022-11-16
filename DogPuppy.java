065aa5c05140be537d1a60cc5c34f67fedc10bd3
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2022-11-16 13:44:45 -0800

    Finnished Instance variable 11/16

--- DogPuppy.java
+++ DogPuppy.java
@@ -7,40 +7,40 @@ public class DogPuppy {
     values need to be retrieved or set from outside the class we should
     define "getter" and "setter" methods.  More on that later
      */
-    private int health;               // An integer that represents
-    private int height;               // An integer that represents
-    private int weight;               // An integer that represents
-    private int energy;               // An integer that represents
-    private int mood;                 // An integer that represents
-    private int strength;             // An integer that represents
+    private int health;             // An integer that represents a dog's health
+    private int height;             // An integer that represents a dog's height
+    private int weight;             // An integer that represents a dog's weight
+    private int energy;             // An integer that represents a dog's energy
+    private int mood;               // An integer that represents a dog's mood
+    private int strength;           // An integer that represents a dog's strength
 
     /* Methods are defined next */
-    public void Walk() {
+    public void walk() {
         health = health + 5;
         energy = energy - 2;
         strength = strength - 2;
     }
 
-    public void Socialize() {
+    public void socialize() {
         energy = energy - 1;
         mood = mood + 2;
     }
 
-    public void Eat() {
+    public void eat() {
         health = health + 5;
         weight = weight + 1;
         energy = energy + 2;
         mood = mood + 5;
     }
 
-    public void Sleep () {
+    public void sleep () {
         health = health + 5;
         energy = energy + 1;
         mood = mood + 2;
         strength = strength + 3;
     }
 
-    public void Growup() {
+    public void growUp() {
         health = health + 10;
         height = height + 50;
         weight = weight + 50;
@@ -48,8 +48,13 @@ public class DogPuppy {
         energy = energy - 5;
     }
 
-    public void EatChocolate() {
+    public void eatChocolate() {
         health = health - 1000;
+    }
+    
+    public void fart() {
+        mood = mood + 5;
+        weight = weight - 1;
     }
 
 }
